44cdcd5f8ea33d829b9136ab4cc83ded35fc8c06
bilarslan <bilarslan@windowslive.com>
2020-09-13 13:38:47 +0300

    Updates

---
+++ precache-manifest.e84cd1f5e940bc0f6abac3fbfbf31dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc6f9636069a5c9dd5e26eb73e2b5aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee8d1313fddbea0a51f",
+    "url": "/static/css/main.9e7e7d01.chunk.css"
+  },
+  {
+    "revision": "33262c8ac95d9f05d4a3",
+    "url": "/static/js/2.4c56537a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4c56537a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee8d1313fddbea0a51f",
+    "url": "/static/js/main.1a2757ca.chunk.js"
+  },
+  {
+    "revision": "c418d659a1a507f6eb59",
+    "url": "/static/js/runtime-main.dfece3e5.js"
+  }
+]);
